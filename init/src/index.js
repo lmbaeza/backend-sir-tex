386b912a50092b0027e71c3a1c991e1253ae9646
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-06-04 10:04:14 -0500

    feat: initializations script was added

---
+++ init/src/index.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+const assert = require("node:assert");
+
+const URL_BASE = process.env.URL || "http://localhost:3000";
+
+(async () => {
+    try {
+        const user1 = await axios.post(URL_BASE + "/user", {
+            "first_name": "Luis Miguel",
+            "last_name": "Báez Aponte",
+            "id_type": "CC",
+            "id": 1032503371,
+            "insentive": [
+                {
+                    "point": 15,
+                    "weight": 1000
+                }
+            ],
+            "username": "lmbaeza",
+            "password": "password"
+        });
+        assert.equal(user1.status, 201);
+        assert.equal(user1.statusText, "Created");
+        console.log(user1.data);
+
+        const user2 = await axios.post(URL_BASE + "/user", {
+            "first_name": "administrator",
+            "last_name": "I",
+            "id_type": "CC",
+            "id": 123,
+            "role": "ADMIN",
+            "insentive": [],
+            "username": "admin",
+            "password": "password"
+        });
+        assert.equal(user2.status, 201);
+        assert.equal(user2.statusText, "Created");
+        console.log(user2.data);
+
+
+
+
+        const container1 = await axios.post(URL_BASE + "/container", {
+            "capacity": 1000,
+            "location": "Universidad Nacional"
+        });
+        assert.equal(container1.status, 201);
+        assert.equal(container1.statusText, "Created");
+        console.log(container1.data);
+
+
+
+        const company1 = await axios.post(URL_BASE + "/company", {
+            "name": "H&M",
+            "location": "Carrera x N° y - z"
+        });
+        assert.equal(company1.status, 201);
+        assert.equal(company1.statusText, "Created");
+        console.log(company1.data);
+    } catch (e) {
+        console.log(e);
+    }
+})();
